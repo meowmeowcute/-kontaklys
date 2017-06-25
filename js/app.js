4ef8f05f6216c91591f4569d2c9b4bcb74f46c55
Liliya Rizvanova <liliya.rizvanova@cxense.com>
2017-06-25 21:06:24 +0400

    Build a Contacts Manager Using Backbone.js: Part 4

--- js/app.js
+++ js/app.js
@@ -34,6 +34,7 @@
         tagName: "article",
         className: "contact-container",
         template: _.template($("#contactTemplate").html()),
+        editTemplate: _.template($("#contactEditTemplate").html()),
 
         render: function () {
             this.$el.html(this.template(this.model.toJSON()));
@@ -41,7 +42,11 @@
         },
 
         events: {
-            "click button.delete": "deleteContact"
+            "click button.delete": "deleteContact",
+            "click button.edit": "editContact",
+            "change select.type": "addType",
+            "click button.save": "saveEdits",
+            "click button.cancel": "cancelEdit"
         },
 
         //delete a contact
@@ -58,6 +63,71 @@
             if (_.indexOf(directory.getTypes(), removedType) === -1) {
                 directory.$el.find("#filter select").children("[value='" + removedType + "']").remove();
             }
+        },
+
+        //switch contact to edit mode
+        editContact: function () {
+            this.$el.html(this.editTemplate(this.model.toJSON()));
+
+            //add select to set type
+            var newOpt = $("<option/>", {
+                html: "<em>Add new...</em>",
+                value: "addType"
+            });
+
+            this.select = directory.createSelect().addClass("type").val(this.$el.find("#type").val()).append(newOpt).insertAfter(this.$el.find(".name"));
+            this.$el.find("input[type='hidden']").remove();
+        },
+
+        addType: function () {
+            if (this.select.val() === "addType") {
+
+                this.select.remove();
+
+                $("<input />", {
+                    "class": "type"
+                }).insertAfter(this.$el.find(".name")).focus();
+            }
+        },
+
+        saveEdits: function (e) {
+            e.preventDefault();
+
+            var formData = {},
+                prev = this.model.previousAttributes();
+
+            //get form data
+            $(e.target).closest("form").find(":input").not("button").each(function () {
+                var el = $(this);
+                formData[el.attr("class")] = el.val();
+            });
+
+            //use default photo if none supplied
+            if (formData.photo === "") {
+                delete formData.photo;
+            }
+
+            //update model
+            this.model.set(formData);
+
+            //render view
+            this.render();
+
+            //if model acquired default photo property, remove it
+            if (prev.photo === "img/placeholder.png") {
+                delete prev.photo;
+            }
+
+            //update contacts array
+            _.each(contacts, function (contact) {
+                if (_.isEqual(contact, prev)) {
+                    contacts.splice(_.indexOf(contacts, contact), 1, formData);
+                }
+            });
+        },
+
+        cancelEdit: function () {
+            this.render();
         }
     });
 
@@ -150,7 +220,6 @@
         addContact: function (e) {
             e.preventDefault();
 
-
             var formData = {};
             $("#addContact").children("input").each(function (i, el) {
                 if ($(el).val() !== "") {
@@ -174,18 +243,16 @@
             var removed = removedModel.attributes;
 
             //if model acquired default photo property, remove it
-            if (removed.photo === "/img/placeholder.png") {
+            if (removed.photo === "img/placeholder.png") {
                 delete removed.photo;
             }
 
             //remove from contacts array
             _.each(contacts, function (contact) {
-
                 if (_.isEqual(contact, removed)) {
                     contacts.splice(_.indexOf(contacts, contact), 1);
                 }
             });
-
         },
 
         showForm: function () {
